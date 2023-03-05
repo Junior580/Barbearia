2a1aae0c1edef72de3c4cc6c9ae4f92874f219c0
Junior580 <junior_silvapereira@hotmail.com>
2023-03-05 20:26:40 -0300

    ++ pnpm

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -20,7 +20,7 @@ const SignUp: React.FC = () => {
       <Content>
         <img src={logoImg} alt="GoBarber" />
 
-        <Form onSubmit={}>
+        <Form onSubmit={handleSubmit}>
           <h1>Faça seu cadastro</h1>
 
           <Input name="name" icon={FiUser} placeholder="Nome" />
